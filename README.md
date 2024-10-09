6a3c90f1f0de64dda1592b565513d3b70bb999b9
Ian Farr <ian.farr@farrworks.com>
2024-10-08 22:17:12 -0400

    latest AstroTag working version. Updates Readme with Images

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# nms-astrotag
-No Mans Sky Screenshot Annotator
+# AstroTag - the No Man's Sky Screenshot Annotator
+
+Welcome to Quol's No Man's Sky screenshot annotater - AstroTag. 
+The purpose of this tool is to annotate screenshots by overlaying information like galaxy name, player name, and notes to the screenshot to help catalog findings. It is recommended that you use Steam's F12 when capturing screenshots as this will capture the portal address, and then the astrotag overlays the other details. AstroTag will not touch the original screenshots, as all edits are performed on copies of the screenshots.
+
+## how it works
+
+This script will simply check the steam screenshot folder for any new screenshots, and then add the annotations to them and save the newly edited screenshot to a new directory. This way the original screenshot is not modified at all. By default, the annotated screenshots will be placed in an "annotated" folder in the same directory as your Steam screenshots using the same name as the original.
+
+When you execute the script, the script will look for screenshots that have not been annotated, and ask you for the details applicable to each screenshot that you would like to overlay, such as galaxy name and some arbitrary note (optional). Galaxy names are entered using their numeric index corresponding to the NMS galaxy, and you can have AstroTag list all galaxies and lookup their corresponding index. 
+
+## Galaxy Names
+All 263 galaxie names are pre-entered. All you have to do is to select the corresponding ID. To see the full list, simply press P at "Enter Galaxy Index" prompt that is displayed when running the script. This will show you the full list. Find the Galaxy, and you will see the index. 
+
+Examples:
+* enter "1" for Euclid
+* enter  "10" for Eissentam
+
+## Requirements
+
+* Windows 10 or 11
+    * powershell (included in windows 10/11)
+    * a powershell execution policy that allows you to run the script
+        * Set-ExecutionPolicy Unrestricted (allows you to run all scripts - be carefull!)
+        * call the script via the following command line:
+            * powershell -ExecutionPolicy Bypass -File nms-astrotag.ps1
+* ImageMagick
+    * download from ImageMagick's site and install or use winget:
+        * https://imagemagick.org/script/download.php
+        * winget install ImageMagick.ImageMagick
+
+
+## installation
+
+Once you have ImageMagick installed, then all that is needed is to download a copy of this script, and place it anywhere you want on your computer. You can download by clicking on the "nms-astrotag.ps1" file above, and then selecting "download" from the "..." menu on the right.
+
+Once downloaded, modify the following variables located at the top of the file (open in any text editor - like notepad or visual studio code):
+
+### $screenShotPath
+this should be set to the patch where steam places your screenshots when you press F12 in-game. if unsure where they are placed, in Steam click on one of your screenshots, and the Steam screenshot manager should open. Click on the little folder icon next to the game dropdown, and that should open the windows file explorer. Update the $screenShotPath varaiable with this location.
+
+### $addPlayerName
+set to $true to add your playername to the top right of your screenshots
+
+### $playerName
+set to your desired player name. this is the name that will be added to the top right corner of your screenshots
+
+## usage
+
+with the file downloaded and variables updated, all that is needed is to execute the script from a powershell console. Open a powershell console, navigate to the correct directory (hint: open the windows file explorer and browse to the folder where you placed the script and then type "powershell" in the navigation bar at the top. this will open powershell in the same directory that file explorer is displaying).
+
+To execute the scipt, enter the following line and press enter:
+
+* .\nms-astrotag.ps1
+
+or you can just start typing "nms" and press your tab key, and the command auto-comlete should take care of the rest for you.
+
+if you do not want to change the default execution policy to unrestricted, then you can still run this script with a command like:
+
+* powershell -ExecutionPolicy Unrestricted -File .\nms-astrotag.ps1
+
+this will cause astrotag to run in the background and locate un-annotated screenshots and prompt you for the correct information that should be overlayed per screenshot.
+
+# Recommendations
+
+It is recommended that you run this script within the new windows terminal which can be downloaded from the microsoft store. This way you can simply click on the screenshot links that AstroTag displays in the console for the image that is currently being processed. Clicking on the link in the windows terminal will open that particular screenshot up in your default image viewer so you can see which screenshot is about to be tagged. Then you can confirm the details and AstroTag will overlay the details and move to the next screenshot.
+
+# Images
+AstroTag will start the main loop upon startup and look for any new screenshots:
+
+![Main Screen](Images/01.png)
+
+When a new screenshot is found, AstroTag displays the path tot he file, and it is clickable if you use the new Windows Terminal which will open up the screenshot in your defatul image viewer. This way you can see which screenshot will be annotated.
+
+You need to confirm the galaxy ID to identify the galaxy. If multiple screenshots will be processed, then the galaxy ID will be reused (although you can change it if required):
+
+![Screenshot Identified Screen](Images/02.png)
+
+Entering 'p' will list all 263 galaxies. You can use the console's scroll bars to browse through the list and identify the ID for your desired galaxy:
+
+![Galaxy List](Images/03.png)
+![Galaxy List continued](Images/04.png)
+
+Enter the desired galaxy ID (such as 10 for Eissentam), and a note (if desired):
+
+![Enter galaxy Id and notes](Images/05.png)
+
+Before AstroTag overlays the information, you need to validate the input. Entering 'n' here will allow  you to change the galaxy ID and note:
+
+![confirm annotations](Images/06.png)
+
+And here is the result that is saved into the annotations directory:
+
+![result](Images/20241005172553_1.jpg)
---
+++ nms-astrotag.ps1
@@ -0,0 +1,459 @@
+$screenShotPath = "D:\Program Files\Steam\userdata\86443413\760\remote\275850\screenshots"
+$playerName = "Quol"
+$addPlayerName = $True
+
+# do not edit anything below this line
+$screenShotAnnotatePath = "$screenShotPath\Annotated"
+
+if (-not(Test-Path -Path $screenShotAnnotatePath -PathType Container)) {
+    New-Item -ItemType Directory -Path $screenShotAnnotatePath | Out-Null
+}
+
+$galaxies = @(
+    "0. Pequibanu",
+    "1. Euclid",
+    "2. Hilbert Dimension",
+    "3. Calypso",
+    "4. Hesperius Dimension",
+    "5. Hyades",
+    "6. Ickjamatew",
+    "7. Budullangr",
+    "8. Kikolgallr",
+    "9. Eltiensleen",
+    "10. Eissentam",
+    "11. Elkupalos",
+    "12. Aptarkaba",
+    "13. Ontiniangp",
+    "14. Odiwagiri",
+    "15. Ogtialabi",
+    "16. Muhacksonto",
+    "17. Hitonskyer",
+    "18. Rerasmutul",
+    "19. Isdoraijung",
+    "20. Doctinawyra",
+    "21. Loychazinq",
+    "22. Zukasizawa",
+    "23. Ekwathore",
+    "24. Yeberhahne",
+    "25. Twerbetek",
+    "26. Sivarates",
+    "27. Eajerandal",
+    "28. Aldukesci",
+    "29. Wotyarogii",
+    "30. Sudzerbal",
+    "31. Maupenzhay",
+    "32. Sugueziume",
+    "33. Brogoweldian",
+    "34. Ehbogdenbu",
+    "35. Ijsenufryos",
+    "36. Nipikulha",
+    "37. Autsurabin",
+    "38. Lusontrygiamh",
+    "39. Rewmanawa",
+    "40. Ethiophodhe",
+    "41. Urastrykle",
+    "42. Xobeurindj",
+    "43. Oniijialdu",
+    "44. Wucetosucc",
+    "45. Ebyeloofdud",
+    "46. Odyavanta",
+    "47. Milekistri",
+    "48. Waferganh",
+    "49. Agnusopwit",
+    "50. Teyaypilny",
+    "51. Zalienkosm",
+    "52. Ladgudiraf",
+    "53. Mushonponte",
+    "54. Amsentisz",
+    "55. Fladiselm",
+    "56. Laanawemb",
+    "57. Ilkerloor",
+    "58. Davanossi",
+    "59. Ploehrliou",
+    "60. Corpinyaya",
+    "61. Leckandmeram",
+    "62. Quulngais",
+    "63. Nokokipsechl",
+    "64. Rinblodesa",
+    "65. Loydporpen",
+    "66. Ibtrevskip",
+    "67. Elkowaldb",
+    "68. Heholhofsko",
+    "69. Yebrilowisod",
+    "70. Husalvangewi",
+    "71. Ovna'uesed",
+    "72. Bahibusey",
+    "73. Nuybeliaure",
+    "74. Doshawchuc",
+    "75. Ruckinarkh",
+    "76. Thorettac",
+    "77. Nuponoparau",
+    "78. Moglaschil",
+    "79. Uiweupose",
+    "80. Nasmilete",
+    "81. Ekdaluskin",
+    "82. Hakapanasy",
+    "83. Dimonimba",
+    "84. Cajaccari",
+    "85. Olonerovo",
+    "86. Umlanswick",
+    "87. Henayliszm",
+    "88. Utzenmate",
+    "89. Umirpaiya",
+    "90. Paholiang",
+    "91. Iaereznika",
+    "92. Yudukagath",
+    "93. Boealalosnj",
+    "94. Yaevarcko",
+    "95. Coellosipp",
+    "96. Wayndohalou",
+    "97. Smoduraykl",
+    "98. Apmaneessu",
+    "99. Hicanpaav",
+    "100. Akvasanta",
+    "101. Tuychelisaor",
+    "102. Rivskimbe",
+    "103. Daksanquix",
+    "104. Kissonlin",
+    "105. Aediabiel",
+    "106. Ulosaginyik",
+    "107. Roclaytonycar",
+    "108. Kichiaroa",
+    "109. Irceauffey",
+    "110. Nudquathsenfe",
+    "111. Getaizakaal",
+    "112. Hansolmien",
+    "113. Bloytisagra",
+    "114. Ladsenlay",
+    "115. Luyugoslasr",
+    "116. Ubredhatk",
+    "117. Cidoniana",
+    "118. Jasinessa",
+    "119. Torweierf",
+    "120. Saffneckm",
+    "121. Thnistner",
+    "122. Dotusingg",
+    "123. Luleukous",
+    "124. Jelmandan",
+    "125. Otimanaso",
+    "126. Enjaxusanto",
+    "127. Sezviktorew",
+    "128. Zikehpm",
+    "129. Bephembah",
+    "130. Broomerrai",
+    "131. Meximicka",
+    "132. Venessika",
+    "133. Gaiteseling",
+    "134. Zosakasiro",
+    "135. Drajayanes",
+    "136. Ooibekuar",
+    "137. Urckiansi",
+    "138. Dozivadido",
+    "139. Emiekereks",
+    "140. Meykinunukur",
+    "141. Kimycuristh",
+    "142. Roansfien",
+    "143. Isgarmeso",
+    "144. Daitibeli",
+    "145. Gucuttarik",
+    "146. Enlaythie",
+    "147. Drewweste",
+    "148. Akbulkabi",
+    "149. Homskiw",
+    "150. Zavainlani",
+    "151. Jewijkmas",
+    "152. Itlhotagra",
+    "153. Podalicess",
+    "154. Hiviusauer",
+    "155. Halsebenk",
+    "156. Puikitoac",
+    "157. Gaybakuaria",
+    "158. Grbodubhe",
+    "159. Rycempler",
+    "160. Indjalala",
+    "161. Fontenikk",
+    "162. Pasycihelwhee",
+    "163. Ikbaksmit",
+    "164. Telicianses",
+    "165. Oyleyzhan",
+    "166. Uagerosat",
+    "167. Impoxectin",
+    "168. Twoodmand",
+    "169. Hilfsesorbs",
+    "170. Ezdaranit",
+    "171. Wiensanshe",
+    "172. Ewheelonc",
+    "173. Litzmantufa",
+    "174. Emarmatosi",
+    "175. Mufimbomacvi",
+    "176. Wongquarum",
+    "177. Hapirajua",
+    "178. Igbinduina",
+    "179. Wepaitvas",
+    "180. Sthatigudi",
+    "181. Yekathsebehn",
+    "182. Ebedeagurst",
+    "183. Nolisonia",
+    "184. Ulexovitab",
+    "185. Iodhinxois",
+    "186. Irroswitzs",
+    "187. Bifredait",
+    "188. Beiraghedwe",
+    "189. Yeonatlak",
+    "190. Cugnatachh",
+    "191. Nozoryenki",
+    "192. Ebralduri",
+    "193. Evcickcandj",
+    "194. Ziybosswin",
+    "195. Heperclait",
+    "196. Sugiuniam",
+    "197. Aaseertush",
+    "198. Uglyestemaa",
+    "199. Horeroedsh",
+    "200. Drundemiso",
+    "201. Ityanianat",
+    "202. Purneyrine",
+    "203. Dokiessmat",
+    "204. Nupiacheh",
+    "205. Dihewsonj",
+    "206. Rudrailhik",
+    "207. Tweretnort",
+    "208. Snatreetze",
+    "209. Iwundaracos",
+    "210. Digarlewena",
+    "211. Erquagsta",
+    "212. Logovoloin",
+    "213. Boyaghosganh",
+    "214. Kuolungau",
+    "215. Pehneldept",
+    "216. Yevettiiqidcon",
+    "217. Sahliacabru",
+    "218. Noggalterpor",
+    "219. Chmageaki",
+    "220. Veticueca",
+    "221. Vittesbursul",
+    "222. Nootanore",
+    "223. Innebdjerah",
+    "224. Kisvarcini",
+    "225. Cuzcogipper",
+    "226. Pamanhermonsu",
+    "227. Brotoghek",
+    "228. Mibittara",
+    "229. Huruahili",
+    "230. Raldwicarn",
+    "231. Ezdartlic",
+    "232. Badesclema",
+    "233. Isenkeyan",
+    "234. Iadoitesu",
+    "235. Yagrovoisi",
+    "236. Ewcomechio",
+    "237. Inunnunnoda",
+    "238. Dischiutun",
+    "239. Yuwarugha",
+    "240. Ialmendra",
+    "241. Reponudrle",
+    "242. Rinjanagrbo",
+    "243. Zeziceloh",
+    "244. Oeileutasc",
+    "245. Zicniijinis",
+    "246. Dugnowarilda",
+    "247. Neuxoisan",
+    "248. Ilmenhorn",
+    "249. Rukwatsuku",
+    "250. Nepitzaspru",
+    "251. Chcehoemig",
+    "252. Haffneyrin",
+    "253. Uliciawai",
+    "254. Tuhgrespod",
+    "255. Iousongola",
+    "256. Odyalutai",
+    "257. Loqvishess (-6)",
+    "258. Enyokudohkiw (-5)",
+    "259. Helqvishap (-4)",
+    "260. Usgraikik (-3)",
+    "261. Hiteshamij (-2)",
+    "262. Uewamoisow (-1)"
+)
+
+function PrintGalaxies {
+    $gs = ""
+    $columns = 0
+    foreach ($g in $galaxies) {
+        $line = $g
+        while ($line.Length -lt 30) {
+            $line += " "
+        }
+
+        if ($columns -lt 3) {
+            $gs += "$line"
+            $columns += 1
+        } else {
+            $gs += "$g`n"
+            $columns = 0
+        }
+    }
+    Write-Host $gs
+    Write-Host ""
+}
+function GetNewScreenShots {
+    $completed = Get-ChildItem -Path $screenShotAnnotatePath -Filter '*.jpg'
+    $screenshots = Get-ChildItem -Path $screenShotPath -Filter '*.jpg'
+    $new = @()
+    foreach ($pic in $screenshots) {
+        if (0 -lt $completed.count -and $completed.Name.Contains($pic.Name)) {
+            continue
+        }
+        $new += $pic
+    }
+    return $new
+}
+
+function GetUserGalaxyInput {
+    while ($true) {
+        $uinput = (Read-Host "Please Enter the galaxy index (p to print galaxy list)").ToLower()
+        if ($uinput -eq 'p') {
+            PrintGalaxies
+        }
+        if ($uinput -eq 'q') {
+            exit
+        }
+        try {
+            $selection = [int]$uinput
+            $galaxyName = $galaxies[$selection]
+            $galaxyShortName = $galaxyName.Split('.')[1].Trim() + " Galaxy"
+            return @($galaxyName, $galaxyShortName)
+        } catch {
+            Write-Host "Please enter a valid galaxy number"
+            continue
+        }
+    }
+}
+
+function GetUserNotesInput {
+    while ($true) {
+        $note = (Read-Host "Please Enter any notes")
+        return $note
+    }
+}
+
+function UsePrevious {
+    param ($Skip = $false)
+   
+    if (-not $Skip) {
+        Write-Host ""
+        Write-Host "Previous Galaxy: $galaxy"
+        Write-Host "Previous Note: $notes"
+        $confirm = (Read-Host "Use Previous details? [y/n]").ToLower()
+        if ($confirm -eq 'y') {
+            return
+        }
+    }
+
+    $script:galaxy, $script:galaxyShortName = GetUserGalaxyInput
+    $script:notes = GetUserNotesInput
+    return
+}
+
+function WriteHeader {
+    Write-Host "--------------------------------------------------"
+    Write-Host "Welcome to AstroTag - the NMS Screenshot Annotator"
+    Write-Host "--------------------------------------------------"
+    Write-Host ""
+    Write-Host "Press Ctrl-C to quit"
+    Write-Host -NoNewline "Looking for new Screenshots"
+}
+
+""
+WriteHeader
+$galaxy = ""
+$galaxyShortName = ""
+$notes = ""
+while ($true) {
+    $screenShotsToAnnotate = GetNewScreenShots
+    if (0 -eq $screenShotsToAnnotate.count) {
+        Write-Host -NoNewline "."
+        Start-Sleep -Seconds 5
+        continue
+    }
+    ""
+    ""
+    "-- Detected $($screenShotsToAnnotate.Count) new screenshots: "
+    $curpic=1
+    foreach ($pic in $screenShotsToAnnotate) {
+        $validated = $false
+        ""
+        "Processig screenshot: $curpic"
+        "    file://localhost/$(($pic.FullName).Replace('\','/').Replace(" ","%20"))"
+        ""
+        if ($galaxyShortName.Length -gt 0) {
+            UsePrevious
+            $validated = $true
+        }
+
+        if ($galaxyShortName.Length -eq 0) {
+            $galaxy, $galaxyShortName = GetUserGalaxyInput
+        } 
+        
+        if ($notes.Length -eq 0) {
+            $notes = GetUserNotesInput
+        } 
+
+        while($true) {
+            ""
+            "Annotating the screenshot with the following details:"
+            "  Galaxy: $galaxyShortName"
+            "  Note: $notes"
+            $proceed = Read-Host "Proceed? [y/n]".ToLower()
+            if ($proceed -eq 'y') {
+                break
+            }
+            UsePrevious -Skip $true
+        }
+        ""
+        ## do the annotation
+
+        # generating the galaxy anotation image
+        magick.exe -size 500x80 xc:none -font Cascadia-Mono-Regular -pointsize 30 `
+        -stroke black -strokewidth 8 -gravity East -annotate +10+5 $galaxyShortName -blur 0x8 `
+        -fill white   -stroke none   -gravity East -annotate +10+5 $galaxyShortName `
+        "$screenShotAnnotatePath\annotate-galaxy.png"
+
+        # overlay the galaxy image
+        magick.exe composite -gravity NorthEast -geometry +10-15 `
+        "$screenShotAnnotatePath\annotate-galaxy.png" $pic.FullName "$screenShotAnnotatePath\$($pic.Name)"
+
+        if ($addPlayerName) {
+            if (-not(Test-Path -Path ("$screenShotAnnotatePath\annotate-player.png") -PathType Leaf)) {
+                # generate the player name image
+                magick.exe -size 500x80 xc:none -font Cascadia-Mono-Regular -pointsize 30 `
+                -stroke black -strokewidth 8 -gravity West -annotate +10+5 $playerName -blur 0x8 `
+                -fill white   -stroke none   -gravity West -annotate +10+5 $playerName `
+                "$screenShotAnnotatePath\annotate-player.png"
+            }
+
+            # overlay the player image
+            magick.exe composite -gravity NorthWest -geometry +5-15 `
+            "$screenShotAnnotatePath\annotate-player.png" "$screenShotAnnotatePath\$($pic.Name)" "$screenShotAnnotatePath\$($pic.Name)"
+        }
+
+        if ($notes.Length -gt 0) {
+            # generate the note image
+            magick.exe -size 1500x80 xc:none -font Cascadia-Mono-Regular -pointsize 25 `
+            -stroke black -strokewidth 8 -gravity East -annotate +10+5 $notes -blur 0x8 `
+            -fill gray   -stroke none   -gravity East -annotate +10+5 $notes `
+            "$screenShotAnnotatePath\annotate-notes.png"
+    
+            # overlay the note image
+            magick.exe composite -gravity SouthEast -geometry +0+15 `
+            "$screenShotAnnotatePath\annotate-notes.png" "$screenShotAnnotatePath\$($pic.Name)" "$screenShotAnnotatePath\$($pic.Name)"
+        }
+
+        # add the border
+        magick.exe "$screenShotAnnotatePath\$($pic.Name)" -border 5  "$screenShotAnnotatePath\$($pic.Name)"
+
+        $curpic+=1
+    }
+
+    Clear-Host
+    WriteHeader
+}
